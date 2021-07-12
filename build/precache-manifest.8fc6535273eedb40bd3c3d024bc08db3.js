3a05525b65a553b907c1ecafacc1114a4d0498b3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-11 17:27:25 -0700

    Initial commit

---
+++ build/precache-manifest.8fc6535273eedb40bd3c3d024bc08db3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3e7a9600d5cbebd49c7352bf0f6d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0886a7bb937185fed374",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2c8ce00d2e991a8afdae",
+    "url": "/static/css/main.b1867ff7.chunk.css"
+  },
+  {
+    "revision": "0886a7bb937185fed374",
+    "url": "/static/js/2.e298ed4a.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.e298ed4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8ce00d2e991a8afdae",
+    "url": "/static/js/main.5a8b26c9.chunk.js"
+  },
+  {
+    "revision": "cb102042a1711cc9e41b",
+    "url": "/static/js/runtime-main.288b0588.js"
+  },
+  {
+    "revision": "dc0b66efc899c8f17c477d02ebdba74a",
+    "url": "/static/media/courses-app-screen.dc0b66ef.png"
+  },
+  {
+    "revision": "f7c3e0d037eec3a5cb83ed1dd671671a",
+    "url": "/static/media/quiz-app-screen.f7c3e0d0.png"
+  },
+  {
+    "revision": "48de87e579919d28f048581065557cb6",
+    "url": "/static/media/resize1.48de87e5.jpg"
+  },
+  {
+    "revision": "a40796d9dcc576073305a9fcaa338a95",
+    "url": "/static/media/wheeler-screen.a40796d9.png"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,75 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const path = require('path')
+const axios = require('axios')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+app.get('/weatherapi', (req, res) => {
+  const WEATHER_API_KEY = process.env.WEATHER_API_KEY
+  const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=45.4712&lon=-122.5988&units=imperial&exclude=minutely&appid=${WEATHER_API_KEY}`
+  axios.get(API_URL)
+    .then(weather => {
+      res.send(weather.data.hourly)      
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/dictionaryapi', (req, res) => {
+  const WEBSTERS_API_KEY = process.env.WEBSTERS_API_KEY
+  const searchTerm = req.query.search
+  const API_URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchTerm}?key=${WEBSTERS_API_KEY}`
+  axios.get(API_URL)
+    .then(response => {
+      res.send(response.data[0].shortdef)
+    })
+})
+
+app.post('/contact', async (req, res) => {
+  const body = req.body
+
+  const smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD
+    }
+  })
+
+  const mailOptions = {
+    from: body.email,
+    to: 'paul.reyneveld@gmail.com',
+    subject: 'Email from PRW',
+    html: `<p>${body.name}</p>
+          <p>${body.email}</p>
+          <p>${body.message}</p>`
+  }
+
+  smtpTransport.sendMail(mailOptions,
+    (err, res) => {
+      if (err) {
+        res.send(err)
+      }
+      else {
+        res.send('Success')
+      }
+      smtpTransport.close()
+    })
+  res.status(200).end()
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'))
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
